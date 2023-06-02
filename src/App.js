2a46e992798c919497b5ff6d0c76bd3351cb9b3d
stanken_a <artyemst3@mail.ru>
2023-06-02 20:08:45 +0300

    feat: add project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './vendor/normalize.css';
+import './vendor/fonts/fonts.css';
+import './App.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
