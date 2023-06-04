ecb96fa4d71297d0867fc5a911c8c4f9bd10e7fc
stanken_a <artyemst3@mail.ru>
2023-06-04 13:11:47 +0300

    fix: move App to components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './vendor/normalize.css';
-import './vendor/fonts/fonts.css';
+import '../../vendor/normalize.css';
+import '../../vendor/fonts/fonts.css';
 import './App.scss';
 
-import Header from './components/Header/Header';
-import Main from './pages/Main/Main';
-import Movies from './pages/Movies/Movies';
+import Header from '../Header/Header';
+import Main from '../../pages/Main/Main';
+import Movies from '../../pages/Movies/Movies';
 
 
 function App() {
   return (
-    <div className="page">
+    <div className="App">
       <Header />
       <main className="content">
         <Routes>
