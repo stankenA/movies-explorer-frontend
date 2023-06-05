3fad49e1d169e3c909c9d44bf5b5d58ec47f35ee
stanken_a <artyemst3@mail.ru>
2023-06-05 17:19:31 +0300

    fix: fix piexl perfect

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -4,44 +4,44 @@ import './AboutProject.scss';
 
 export default function AboutProject() {
   return (
-    <section className="about">
-      <div className="about__wrapper">
-        <h2 className="about__title">
+    <section className="project">
+      <div className="project__wrapper">
+        <h2 className="project__title">
           О проекте
         </h2>
-        <div className="about__content">
-          <div className="about__column">
-            <h3 className="about__subtitle">
+        <div className="project__content">
+          <div className="project__column">
+            <h3 className="project__subtitle">
               Дипломный проект включал 5 этапов
             </h3>
-            <p className="about__description">
+            <p className="project__description">
               Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
             </p>
           </div>
-          <div className="about__column">
-            <h3 className="about__subtitle">
+          <div className="project__column">
+            <h3 className="project__subtitle">
               На выполнение диплома ушло 5 недель
             </h3>
-            <p className="about__description">
+            <p className="project__description">
               У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
             </p>
           </div>
         </div>
-        <div className="about__plan">
-          <div className="about__backend">
-            <p className="about__txt about__txt_black">
+        <div className="project__plan">
+          <div className="project__backend">
+            <p className="project__txt project__txt_black">
               1 неделя
             </p>
           </div>
-          <div className="about__frontend">
-            <p className="about__txt">
+          <div className="project__frontend">
+            <p className="project__txt">
               4 недели
             </p>
           </div>
-          <p className="about__txt about__txt_grey">
+          <p className="project__txt project__txt_grey">
             Back-end
           </p>
-          <p className="about__txt about__txt_grey">
+          <p className="project__txt project__txt_grey">
             Front-end
           </p>
         </div>
